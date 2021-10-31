3d90467996b079a7e0adba8486c4c2f3aed6962a
Rahul <rahulbehera2011@gmail.com>
2021-10-31 23:26:26 +0530

    Added files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  const title = 'Welcome to the new blog'
+  const likes = 50
+  return (
+    <div className="App">
+      <div className="content">
+        <h1>{title}</h1>
+        <p>Like {likes} times</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
