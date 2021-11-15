5c9a62e761bf964ca7b36bb7c65e2877347287f3
Rahul <rahulbehera2011@gmail.com>
2021-11-16 00:45:02 +0530

    Counter Question

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
-
+import InterviewQuestions from './interviewQuestions.js'
 function App() {
-  const title = 'Welcome to the new blog'
-  const likes = 50
+  const title = 'Welcome to JS Interview'
+  const source = 'https://www.youtube.com/watch?v=gnkrDse9QKc&ab_channel=BenAwad'
   return (
     <div className="App">
       <div className="content">
         <h1>{title}</h1>
-        <p>Like {likes} times</p>
+        <a href={source}>Ben Awad JS Interview</a>
       </div>
+      <InterviewQuestions/>
     </div>
   );
 }
