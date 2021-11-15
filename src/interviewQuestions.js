dbe8f2f8132d9b26ba5058eb2654dc5c5c29cc5c
Rahul <rahulbehera2011@gmail.com>
2021-11-16 00:51:07 +0530

    Biolder plate code

--- src/interviewQuestions.js
+++ src/interviewQuestions.js
@@ -1,18 +1,5 @@
-import { useState } from "react";
 const InterviewQuestions = () => {
-  const [count, setCount] = useState(0);
-  return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "space-evenly",
-        flexDirection: "row-reverse"
-      }}
-    >
-      <div>{"Count: " + count}</div>
-      <button onClick={() => setCount(count + 1)}>Count++</button>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default InterviewQuestions;
